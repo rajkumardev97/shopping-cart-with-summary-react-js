081d167c5302a2b4526df8a7e4833f70e31e39b1
rajkumardev97 <developer.rk97@gmail.com>
2019-05-14 19:24:34 +0530

    remove useless code

--- config/keys.js
+++ config/keys.js
@@ -2,11 +2,3 @@ module.exports = {
   mongoURI: "mongodb://localhost/shopping",
   secretOrKey: "secret"
 };
-
-{
-  /*mongoURI:
-      "mongodb://princead:prince12345@ds129024.mlab.com:29024/inventorybuddydb",
-  
-      mongoURI: "mongodb://localhost/inventorybuddydb"
-    */
-}
